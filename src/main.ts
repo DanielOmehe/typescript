0238a90f85e1aa9b6b6b93e44476d40cf2a05a25
DaniOmehe <danielomehe66@gmail.com>
2023-05-11 17:54:50 +0100

    typescript terminiologies

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,39 @@ let age: number = 100;
 
 console.log(age);
 
-let a = 12;
-let b = 6;
-let c = 3;
+let a: number = 12;
+let b: number = 6;
+let c: number = 3;
+let d: string = 'some text';
+let e: number = 12;
+
+console.log(a / b);
+console.log(b / c);
+console.log(d);
+console.log(e);
+
+let myName: string;
+let meaningOfLife: number;
+let isLoading: boolean;
+
+myName = 'hello';
+meaningOfLife = 12;
+isLoading = false
+
+console.log(myName);
+console.log(meaningOfLife);
+console.log(isLoading);
+
+let result: any;
+let some: string | number;
+
+const sum = (a: number, b: number)=>{
+    some = 'true'
+    return a + some + b
+}
+
+let reg: RegExp = /\w+/g;
+
+console.log(sum(10, 20));
+
+
