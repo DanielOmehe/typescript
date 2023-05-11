444d721a5e0b1f2964e49537e5f9986c8565eae1
DaniOmehe <danielomehe66@gmail.com>
2023-05-11 17:31:05 +0100

    Typescript practice

---
+++ build/js/main.js
@@ -0,0 +1,9 @@
+"use strict";
+let text = 'hello world';
+console.log(text);
+let word = 'hello there';
+let age = 100;
+console.log(age);
+let a = 12;
+let b = 6;
+let c = 3;
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+let text: string = 'hello world';
+
+console.log(text)
+
+let word = 'hello there'
+
+let age: number = 100;
+
+console.log(age);
+
+let a = 12;
+let b = 6;
+let c = 3;
