10fd54bac1f20e8c3b012a2a98f25de6cdbd6a32
DaniOmehe <danielomehe66@gmail.com>
2023-05-11 18:29:48 +0100

    Typescript objects and enums

--- build/js/main.js
+++ build/js/main.js
@@ -1,13 +1,13 @@
 "use strict";
-let text = 'hello world';
+let text = "hello world";
 console.log(text);
-let word = 'hello there';
+let word = "hello there";
 let age = 100;
 console.log(age);
 let a = 12;
 let b = 6;
 let c = 3;
-let d = 'some text';
+let d = "some text";
 let e = 12;
 console.log(a / b);
 console.log(b / c);
@@ -16,7 +16,7 @@ console.log(e);
 let myName;
 let meaningOfLife;
 let isLoading;
-myName = 'hello';
+myName = "hello";
 meaningOfLife = 12;
 isLoading = false;
 console.log(myName);
@@ -25,8 +25,58 @@ console.log(isLoading);
 let result;
 let some;
 const sum = (a, b) => {
-    some = 'true';
+    some = "true";
     return a + some + b;
 };
 let reg = /\w+/g;
 console.log(sum(10, 20));
+let stringArray = ["dave"];
+let guitars = ["strat", "les paul", 5150];
+let mixedData = ["dave", 1987, true];
+stringArray[0] = "10";
+stringArray.push("34");
+console.log(stringArray);
+guitars = stringArray;
+guitars[0] = "something";
+console.log(guitars);
+let bands = [];
+bands.push("something");
+console.log(bands);
+//tuple
+let myTuple = ["somethings", 12, false];
+mixedData = myTuple;
+mixedData.push(123);
+myTuple.push(123);
+console.log(mixedData);
+console.log(myTuple);
+let myObject = {};
+myObject = [];
+myObject = sum;
+console.log(myObject);
+let jon = {
+    //   name: "Jon bellion",
+    active: false,
+    albums: ["All time low", 80, "hello"],
+};
+let nick = {
+    name: "Nick Jonas",
+    albums: ["close"],
+};
+const greetArtiste = (artiste) => {
+    if (artiste.name) {
+        return ("Hello " + artiste.name + " i love your song " + artiste.albums.join(", "));
+    }
+    return "Hello!";
+};
+console.log(greetArtiste(nick));
+console.log(greetArtiste(jon));
+//Enums
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 3] = "U";
+    Grade[Grade["D"] = 4] = "D";
+    Grade[Grade["C"] = 5] = "C";
+    Grade[Grade["B"] = 6] = "B";
+    Grade[Grade["A"] = 7] = "A";
+})(Grade || (Grade = {}));
+console.log(Grade.A);
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-let text: string = 'hello world';
+let text: string = "hello world";
 
-console.log(text)
+console.log(text);
 
-let word = 'hello there'
+let word = "hello there";
 
 let age: number = 100;
 
@@ -11,7 +11,7 @@ console.log(age);
 let a: number = 12;
 let b: number = 6;
 let c: number = 3;
-let d: string = 'some text';
+let d: string = "some text";
 let e: number = 12;
 
 console.log(a / b);
@@ -23,9 +23,9 @@ let myName: string;
 let meaningOfLife: number;
 let isLoading: boolean;
 
-myName = 'hello';
+myName = "hello";
 meaningOfLife = 12;
-isLoading = false
+isLoading = false;
 
 console.log(myName);
 console.log(meaningOfLife);
@@ -34,13 +34,90 @@ console.log(isLoading);
 let result: any;
 let some: string | number;
 
-const sum = (a: number, b: number)=>{
-    some = 'true'
-    return a + some + b
-}
+const sum = (a: number, b: number) => {
+  some = "true";
+  return a + some + b;
+};
 
 let reg: RegExp = /\w+/g;
 
 console.log(sum(10, 20));
 
+let stringArray = ["dave"];
+let guitars = ["strat", "les paul", 5150];
+let mixedData = ["dave", 1987, true];
+
+stringArray[0] = "10";
+stringArray.push("34");
+
+console.log(stringArray);
+
+guitars = stringArray;
+
+guitars[0] = "something";
+console.log(guitars);
+
+let bands: string[] = [];
+
+bands.push("something");
+console.log(bands);
+
+//tuple
+let myTuple: [string, number, boolean] = ["somethings", 12, false];
+
+mixedData = myTuple;
+mixedData.push(123);
+myTuple.push(123);
+
+console.log(mixedData);
+console.log(myTuple);
+
+let myObject: object = {};
+
+myObject = [];
+
+myObject = sum;
+
+console.log(myObject);
+
+interface Artiste {
+  name?: string;
+  active?: boolean;
+  albums: (string | number)[];
+}
+
+let jon: Artiste = {
+//   name: "Jon bellion",
+  active: false,
+  albums: ["All time low", 80, "hello"],
+};
+
+let nick: Artiste = {
+  name: "Nick Jonas",
+  albums: ["close"],
+};
+
+const greetArtiste = (artiste: Artiste) => {
+  if (artiste.name) {
+    return (
+      "Hello " + artiste.name + " i love your song " + artiste.albums.join(", ")
+    );
+  }
+  return "Hello!"
+};
+
+console.log(greetArtiste(nick));
+console.log(greetArtiste(jon));
+
+//Enums
+
+enum Grade{
+    U = 3,
+    D,
+    C,
+    B,
+    A
+}
+
+console.log(Grade.A);
 
